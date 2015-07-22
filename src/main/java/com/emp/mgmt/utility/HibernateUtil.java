d45b9b10d5ade946976b83599fe1f6a75f45a11a
Priya <priya28.maheshwari@gmail.com>
2015-07-22 13:54:05 +0530

    Added more session transactions

--- src/main/java/com/emp/mgmt/utility/HibernateUtil.java
+++ src/main/java/com/emp/mgmt/utility/HibernateUtil.java
@@ -1,14 +1,17 @@
 package com.emp.mgmt.utility;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
  
+@SuppressWarnings("deprecation")
 public class HibernateUtil {
     private static final SessionFactory sessionFactory;
     static {
         try {
             sessionFactory = new Configuration().configure()
                     .buildSessionFactory();
+            sessionFactory = new Configuration().configure().buildSessionFactory();
         } catch (Throwable ex) {
             System.err.println("SessionFactory creation failed" + ex);
             throw new ExceptionInInitializerError(ex);
@@ -17,5 +20,33 @@ public class HibernateUtil {
  
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
+    }
+    
+    public static Session getSession()
+    {
+    	Session hibernateSession = sessionFactory.getCurrentSession();
+    	return hibernateSession;
+    }
+    
+    public static Session beginTransaction()
+    {
+    	Session hibernatSession = HibernateUtil.getSession();
+    	hibernatSession.beginTransaction();
+    	return hibernatSession;
+    }
+    
+    public static void commitTransaction()
+    {
+    	HibernateUtil.getSession().getTransaction().commit();
+    }
+    
+    public static void rollbackTransaction()
+    {
+    	HibernateUtil.getSession().getTransaction().rollback();
+    }
+    
+    public static void closeTransaction()
+    {
+    	HibernateUtil.getSession().close();
     }
 }
